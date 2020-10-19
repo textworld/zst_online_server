92895b67f6480d13e271443a15a8dae66e9dd96d
textworld <text.zwb@gmail.com>
2020-10-19 23:09:14 +0800

    fix: 删除了一些旧代码

--- zst_project/urls.py
+++ zst_project/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('course.urls')),
-    path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('user.urls')),
     path('api/v1/bill/', include('bill.urls'))
 ]
