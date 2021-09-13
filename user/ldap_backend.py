8179350156dddbe6ee7430f66a65ae9ce08c0d32
textworld <text.zwb@gmail.com>
2021-09-13 12:39:49 +0800

    feat: 优化docker-compose

--- user/ldap_backend.py
+++ user/ldap_backend.py
@@ -38,8 +38,7 @@ class LdapBackend:
                     built = False
 
                 if built:
-                    user.set_password()
-                    #user.set_unusable_password()
+                    user.set_unusable_password()
                     user.save()
                 return user
         except ldap.INVALID_CREDENTIALS:
